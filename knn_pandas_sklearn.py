6a202fd0f0b6e55c0af8bd29f032158197010053
Håkan Pettersson <hakan.pettersson.mmx@gmail.com>
2021-10-06 11:19:16 +0200

    My own implementation of kNN.

---
+++ knn_pandas_sklearn.py
@@ -0,0 +1,126 @@
+import math
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+np.set_printoptions(formatter={'float': lambda x: "{0:.1f}".format(x).rjust(4)})
+
+def read_data(read_from_file=True):
+    names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'Class']
+    if read_from_file:
+        import pathlib
+        filename = 'iris.data'  # Located in the same directory as the current file.
+        full_filename = str(pathlib.Path().absolute().joinpath(filename))
+        df = pd.read_csv(full_filename, names=names)  # pandas.core.frame.DataFrame
+    else:
+        url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+        df = pd.read_csv(url, names=names)  # pandas.core.frame.DataFrame
+
+    X = df.iloc[:, :-1].values
+    y = df.iloc[:, 4].values
+
+    return X, y
+
+
+X, y = read_data()
+# print(X.shape); print(y.shape); exit()
+
+# print(dataset.head(3));print();print(X[:3]);print();print(y[:3])
+
+# from sklearn.model_selection import train_test_split
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
+
+from my_splitter import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
+
+# print(y_test);exit()
+
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+scaler.fit(X_train)
+
+X_train = scaler.transform(X_train)
+X_test = scaler.transform(X_test)
+
+# print(X[:5]); print(); print(X_train[:5]); print()
+from math import *
+from dataclasses import dataclass, field
+
+@dataclass
+class DistanceData:
+    features: list
+    target: str
+    distance: float
+
+@dataclass
+class PredictionData:
+    predicted: str
+    features: list  # The features of a test sample. Used for debug.
+    neighbors: list
+
+
+# exit()
+from statistics import mode
+class MyClassifier:
+    def __init__(self, n_neighbors=5):
+        self.n_neighbors = n_neighbors
+
+    def fit(self, X_train, y_train):
+        self.distance_data_list = []
+        self.predicted_list = []
+        for x, y in zip(X_train, y_train):
+            dd = DistanceData(x, y, -1)
+            self.distance_data_list.append(dd)
+
+    def predict_row(self, y_test_row):
+        # Calculate distance to every sample in X_train.
+        for dd in self.distance_data_list:
+            squared = [(x1 - x2) * (x1 - x2) for x1, x2 in zip(dd.features, y_test_row)]
+            dd.distance = math.sqrt(sum(squared))
+
+        # Select the k closest neighbors.
+        sorted_by_distance = sorted(self.distance_data_list, key=lambda x: x.distance)
+        k_neighbors = sorted_by_distance[:self.n_neighbors]
+
+        # Select most frequent neighbor of the k closest.
+        predicted = mode([x.target for x in k_neighbors])
+        tg = PredictionData(predicted, y_test_row, k_neighbors)
+        self.predicted_list.append(tg)
+
+    def predict(self, X_test):
+        for test_row in X_test:
+            self.predict_row(test_row)
+
+        # for elem in self.predicted_list:
+        #     print(elem.features, elem.predicted)
+
+        y_pred = [x.predicted for x in self.predicted_list]
+        return y_pred
+
+c = MyClassifier(5)
+c.fit(X_train, y_train)
+my_y_pred = c.predict(X_test)
+print('my_y_pred')
+for single_y_pred in my_y_pred:
+    print(single_y_pred)
+print()
+# exit()
+
+from sklearn.neighbors import KNeighborsClassifier
+classifier = KNeighborsClassifier(n_neighbors=5)
+classifier.fit(X_train, y_train)
+
+y_pred = classifier.predict(X_test)
+print('y_pred')
+for single_y_pred in y_pred:
+    print(single_y_pred)
+print()
+exit()
+
+from sklearn.metrics import classification_report, confusion_matrix
+print(confusion_matrix(y_test, y_pred))
+print()
+print(classification_report(y_test, y_pred))
+
+
+
--- main.py
+++ main.py
@@ -1,28 +1,11 @@
 import my_iris
-# import numpy as np
-#
-# def train_test_split(X, y, test_size=0.33, random_state=None):
-#     indexes = np.array(range(len(X)))
-#     np.random.seed(random_state)
-#     np.random.shuffle(indexes)
-#     test_count = round(len(indexes) * test_size)
-#
-#     test_indexes = indexes[:test_count]
-#     train_indexes = indexes[test_count:]
-#
-#     X_train = X[train_indexes]
-#     X_test = X[test_indexes]
-#     y_train = y[train_indexes]
-#     y_test = y[test_indexes]
-#
-#     return X_train, X_test, y_train, y_test
-
+import my_splitter as ms
 
 iris= my_iris.Iris.load_iris()
 X = iris.data
 y = iris.target
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
+X_train, X_test, y_train, y_test = ms.train_test_split(X, y, test_size=0.1, random_state=42)
 print(f'X_train.shape: {X_train.shape}')
 print(f'X_test.shape: {X_test.shape}')
 
