f23461eb576bc427984389a21efb23d0000e0b12
Håkan Pettersson <hakan.pettersson.mmx@gmail.com>
2021-10-07 15:56:17 +0200

    Added missing files.

---
+++ my_classifier.py
@@ -0,0 +1,53 @@
+import math
+from statistics import mode
+from dataclasses import dataclass
+
+@dataclass
+class TrainingData:
+    features: list      # Features of a training sample
+    label: str          # Label of a training sample
+    distance: float     # Distance between a test sample and this trainings sample
+
+
+@dataclass
+class PredictionData:
+    predicted_label: str
+    features: list      # The features of a test sample. Used for debug.
+    neighbors: list     # The features of the k nearest neighbors. Used for debug.
+
+
+class MyClassifier:
+    def __init__(self, n_neighbors=5):
+        self.n_neighbors = n_neighbors
+        self.distance_data_list = []
+        self.prediction_data_list = []
+
+    def fit(self, X_train, y_train):
+        for features, label in zip(X_train, y_train):
+            td = TrainingData(features, label, -1)
+            self.distance_data_list.append(td)
+
+    def predict_test_sample(self, test_sample_features):
+        # Calculate distance to every sample in X_train.
+        for dd in self.distance_data_list:
+            squared = [(x1 - x2) * (x1 - x2) for x1, x2 in zip(dd.features, test_sample_features)]
+            dd.distance = math.sqrt(sum(squared))
+
+        # Select the k closest neighbors.
+        sorted_by_distance = sorted(self.distance_data_list, key=lambda x: x.distance)
+        k_neighbors = sorted_by_distance[:self.n_neighbors]
+
+        # Select most frequent neighbor of the k closest.
+        predicted_label = mode([x.label for x in k_neighbors])
+        pd = PredictionData(predicted_label, test_sample_features, k_neighbors)
+        self.prediction_data_list.append(pd)
+
+    def predict(self, X_test):
+        for test_sample in X_test:
+            self.predict_test_sample(test_sample)
+
+        # for elem in self.prediction_data_list:
+        #     print(elem.features, elem.predicted)
+
+        y_pred = [x.predicted_label for x in self.prediction_data_list]
+        return y_pred
